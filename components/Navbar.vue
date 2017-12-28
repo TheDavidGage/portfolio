ae05d7c7fe01f14fbe71d293670cde8d1a1ca727
David Gage Hall <FearlessDavidGage@TheDavidGage.local>
2017-12-28 11:39:59 -0500

    navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">Portfolio</a>
+        <button class="button navbar-burger">
+          <span></span>
