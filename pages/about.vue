2b06becdae4079598c5c04bcece28623e12e725f
David Gage Hall <davidgage.hall@gmail.com>
2017-12-28 16:50:44 -0500

    commits

--- pages/about.vue
+++ pages/about.vue
@@ -5,6 +5,13 @@
       <p>
         My name is David Gage Hall. I'm a self taught web developer based in Marquette, MI. I build modern applications for the web.
       </p>
+      	<hr>
+        
+        <section>
+		      <h3 class="title">Some of my work.</h3>
+		      <hr>
+          <h4>Hello</h4>
+        </section>
     </div>
   </section>
 </template>
