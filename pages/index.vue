9090d9c05fbe4d5d6bca7349a6b270751b6b6c8f
David Gage Hall <FearlessDavidGage@TheDavidGage.local>
2017-12-28 11:30:05 -0500

    brought in bulma

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         portfolio
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        a portfolio project
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
